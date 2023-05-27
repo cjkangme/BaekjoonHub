4f6d37d055933b20091fa5752bde7abada121998
cjkangme <nureongi0214@gmail.com>
2023-05-27 10:42:18 +0900

    [Gold IV] Title: 가르침, Time: 2144 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.py
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -3,28 +3,29 @@ from itertools import combinations
 
 input = sys.stdin.readline
 
-def get_count(words, known, max_count):
-    count = 0
-    for idx, word in enumerate(words):
-        if N - idx + count <= max_count:
-            return count
-        for char in word:
-            if char not in known:
-                break
-        else:
-            count += 1
-    return count
+def word2bit(word):
+    bit = 0
+    for char in word:
+        bit = bit | (1 << ord(char) - ord('a'))
+    return bit
 
 N, K = map(int, input().split())
 words = [input().rstrip() for _ in range(N)]
-base = set(['a', 'n', 't', 'i', 'c'])
-answer = 0
+bits = list(map(word2bit, words))
+base_word = word2bit('antic')
+
 if K < 5:
     print(0)
 else:
     alphabet = set([chr(i) for i in range(ord('a'), ord('z')+1)])
-    alphabet = alphabet - base
+    alphabet = alphabet - set(['a', 'n', 't', 'i', 'c'])
+    answer = 0
     for combination in combinations(alphabet, K-5):
-        temp = set(combination) | base
-        answer = max(answer, get_count(words, temp, answer))
+        know_word = list(combination)
+        know_bit = base_word | word2bit(know_word)
+        count = 0
+        for bit in bits:
+            if bit & know_bit == bit:
+                count += 1
+        answer = max(answer, count)
     print(answer)
