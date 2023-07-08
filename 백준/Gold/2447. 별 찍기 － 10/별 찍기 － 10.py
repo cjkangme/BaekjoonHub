7669c6479bbef7f49587f175e03a25e53124b1d9
cjkangme <nureongi0214@gmail.com>
2023-07-08 23:18:06 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 3016 ms, Memory: 35872 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,18 +1,24 @@
-def is_blank(x, y, devider):
-    if devider == 0:
-        return False
+def draw(n):
+    if n==1:
+        return ["*"]
     
-    if x // devider == 1 and y // devider == 1:
-        return True
-    else:
-        return is_blank(x % devider, y % devider, devider//3)
+    stars = draw(n//3)
+    board = []
+    
+    for star in stars:
+        board.append(star * 3)
+    for star in stars:
+        board.append(star + " "*(n//3) + star)
+    for star in stars:
+        board.append(star * 3)
+    
+    return board
 
-N = int(input())
+if __name__ == "__main__":
+    N = int(input())
+    answer = draw(N)
     
-for i in range(N):
-    for j in range(N):
-        if is_blank(i, j, N//3):
-            print(" ", end="")
-        else:
-            print("*", end="")
-    print()
+    for i in range(N):
+        for j in range(N):
+            print("".join(answer[i][j]), end="")
+        print()
