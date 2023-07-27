fa599465ff89ecdc4dfed1495ad77c2e1a6d5a03
cjkangme <nureongi0214@gmail.com>
2023-07-27 11:48:31 +0900

    [Silver II] Title: 편의점 2, Time: 224 ms, Memory: 39352 KB -BaekjoonHub

--- 백준/Silver/14400. 편의점 2/편의점 2.py
+++ 백준/Silver/14400. 편의점 2/편의점 2.py
@@ -1,35 +1,5 @@
 import sys
 
-# 병합 정렬
-def merge_sort(arr, left, right):
-    if left == right:
-        return arr
-    
-    mid = (left + right) // 2
-    lmid, rmid = mid, mid+1
-    arr = merge_sort(arr, left, lmid)
-    arr = merge_sort(arr, rmid, right)
-    
-    l, r = left, rmid
-    temp = []
-    while l <= lmid and r <= right:
-        if arr[l] <= arr[r]:
-            temp.append(arr[l])
-            l += 1
-        else:
-            temp.append(arr[r])
-            r += 1
-
-    for i in range(l, lmid+1):
-        temp.append(arr[i])
-
-    for i in range(r, right+1):
-        temp.append(arr[i])
-    
-    arr[left:right+1] = temp
-    
-    return arr
-
 input = sys.stdin.readline
 
 if __name__ == "__main__":
@@ -44,11 +14,11 @@ if __name__ == "__main__":
         y_list.append(y)
     
     # 정렬
-    x_sorted = merge_sort(x_list, 0, n-1)
-    y_sorted = merge_sort(y_list, 0, n-1)
+    x_list.sort()
+    y_list.sort()
     
     # 최소거리 위치 찾기
-    x_point, y_point = x_sorted[n//2], y_sorted[n//2]
+    x_point, y_point = x_list[n//2], y_list[n//2]
     
     # 위치 구하기
     for x, y in zip(x_list, y_list):
